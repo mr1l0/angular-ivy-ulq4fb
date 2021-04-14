e9aefb271ecc2e850310cc1403f66f3494d22eed
mr1l0 <muriloamaral1@gmail.com>
2021-04-14 08:10:36 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { MatButtonModule } from "@angular/material/button";
+
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { AuthViewComponent } from "./auth-view/auth-view.component";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  declarations: [AppComponent, HelloComponent, AuthViewComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
